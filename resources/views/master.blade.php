29b478d556d3c7d83bee3b16601e755d34744f5b
linhnv296 <linhnv296@gmail.com>
2021-09-27 22:54:46 +0700

    refactor UI/UX

---
+++ resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Point CRM.</title>
+    <meta name="description" content="Point CRM."/>
+</head>
+<body>
+    <div>
+        @yield('content')
+        @include('footer')
+    </div>
+</body>
+</html>
